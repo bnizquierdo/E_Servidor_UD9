c763360576dff9ddf0d61b802cecf7e2952aa7ee
Belén Izquierdo dorado <59977724+bnizquierdo@users.noreply.github.com>
2022-03-07 22:35:49 +0100

    Update pantalon.php

--- pantalon.php
+++ pantalon.php
@@ -28,11 +28,12 @@
     </div>
 
  
-    <!--categorias-->
+    <!--catalogo-->
     <div class="small-container">
         <h1 class="title">Pantalones</h1>
         <div class="row">
             <div class="col-4">
+                <!--Formularios para enviar los datos mediante el método post-->
                 <form method="post" action="carro.php?nombre=Pantalon_Gris&precio=20">
                     <img src="img/product-3.jpg" alt="">
                     <h4>Pantalón gris</h4>
